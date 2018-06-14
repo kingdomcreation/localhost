65df78fa8ab66ca07cbbb2beef4568c8579920e8
kingdomcreation <kingdomcreation@gmail.com>
2018-06-14 18:35:10 -0400

    Load pages fragments using ajax

--- js/ajax.js
+++ js/ajax.js
@@ -3,19 +3,41 @@ console.log('ajax.js loaded');
 $('form[action]').on('submit',function(e){
   e.preventDefault();
   
+  var reload = {dataType: "script",cache: true};
   var url = $(this).attr('action');
   console.log("prevented submit from " + url);
   
-  // Use jQuery $.ajax(url,{options}) method
+  $.ajax(url+"&ajax", {
+    dataType: "html",
+    method: 'POST',
+    data: $(this).serialize()+'&action=new',
+    success: function(data){
+      console.log(data);
+      $('div.container').html(data);
+      // Re-attach ajax and restart client
+      $.ajax('/js/ajax.js',reload);
+      $.ajax('/js/main.js',reload);
+    }
+  });
   
 });
 
 $('a[href]').on('click',function(e){
   e.preventDefault();
   
+  var reload = {dataType: "script",cache: true};
   var url = $(this).attr('href');
   console.log("prevented click from " + url);
   
-  // Use jQuery $.ajax(url,{options}) method
+  $.ajax(url+"?ajax",{
+    dataType: "html",
+    success: function(data){
+      console.log(data);
+      $('div.container').html(data);
+      // Re-attach ajax and restart client
+      $.ajax('/js/ajax.js',reload);
+      $.ajax('/js/main.js',reload);
+    }
+  });
   
 });
