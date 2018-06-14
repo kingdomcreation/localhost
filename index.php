493ec99ec38c461f04782e8a317ac77d543c415c
kingdomcreation <kingdomcreation@gmail.com>
2018-06-13 21:12:23 -0400

    Add slug function

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <?php define('PHP_','php/'); include(PHP_.'functions.php'); 
-$index = 'index';
+$index = $data['url'] = slug();
 $pages = [
+  'about'=>"About Us",
+  'contact'=>"Contact Form",
   'index'=>"Project name"
 ];
+if(!isset($pages[$index])){
+  $index = 'index'; 
+}
 $title = $data['title'] = $pages[$index];
 
 index($data);
--- php/functions.php
+++ php/functions.php
@@ -1,4 +1,8 @@
 <?php !defined(PHP_) OR die('no access');
+function slug(){
+    return trim(str_replace($_SERVER['SCRIPT_NAME'],'',
+      parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)),'/');
+}
 
 function index(&$data=['title'=>""],$include=''){
   global $index;
