3dd42366552293de1095c7324437c9cddaf3192a
kingdomcreation <kingdomcreation@gmail.com>
2018-06-16 18:17:39 -0400

    Load json data when client submit form

--- js/ajax.js
+++ js/ajax.js
@@ -3,26 +3,38 @@ console.log('ajax.js loaded');
 $('form[action]').on('submit',function(e){
   e.preventDefault();
   
-  var reload = {dataType: "script",cache: true};
+  var form = $(this);
   var url = $(this).attr('action');
   console.log("prevented submit from " + url);
   
-  $.ajax(url+"&ajax", {
-    dataType: "html",
+  $.ajax(url+"&json", {
+    dataType: "json",
     method: 'POST',
-    data: $(this).serialize()+'&action=new',
+    data: {
+      email: $('input[name="email"]').val(),
+      action: "new"
+    },
     success: function(data){
       console.log(data);
-      $('div.container').html(data);
+      if(data.error){
+        $('div.error').fadeOut().remove();
+        form.before('<div class="error">'+data.error+'</div>');
+        form.on('change',function(){
+          $('div.error').fadeOut().remove();
+        })
+      }else if(data.status && data.status=='success'){
+        console.log(data.status);
+        form.html('<a href="../">Back</a>');
+      }
       // Re-attach ajax and restart client
-      $.ajax('/js/ajax.js',reload);
-      $.ajax('/js/main.js',reload);
+      //$.ajax('/js/ajax.js',reload);
+      //$.ajax('/js/main.js',reload);
     }
   });
   
 });
 
-$('a[href]').on('click',function(e){
+$('a[href]').not('[data-toggle]').on('click',function(e){
   e.preventDefault();
   
   var reload = {dataType: "script",cache: true};
