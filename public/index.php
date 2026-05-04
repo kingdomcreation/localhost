7d4f47f7082e49a3ee734cc3d56aaaf49669963d
Kingdom Creation <kingdomcreation@gmail.com>
2026-05-04 14:10:51 -0400

    Move files and folders into public directory

---
+++ public/index.php
@@ -0,0 +1,45 @@
+<?php define("PHP_", "../php/");
+include PHP_ . "functions.php";
+
+$index = $data["url"] = slug();
+
+if ("logout" == $index) {
+    // Destroy session & redirect
+    header("Location: /");
+}
+$pages = [
+    "about" => "About Us",
+    "contact" => "Contact Form",
+    "contact/new" => "New Message",
+    "contact/form" => "New Message",
+    "contact/form/new" => "New Message",
+    "index" => "Project name",
+];
+
+if (isset($_GET["p"]) && isset($pages[$_GET["p"]])) {
+    $index = $_GET["p"];
+} else {
+    $index = empty($index) ? "index" : $index;
+}
+$title = $data["title"] = $pages[$index];
+
+if (isset($_POST["action"])) {
+    $action = isset($pages[$index . "/" . $_POST["action"]]) ? $_POST["action"] : false;
+}
+
+if (isset($action)) {
+    $form = include PHP_ . $index . "-handler.php";
+    if (is_array($form)) {
+        $data = array_merge($data, $form);
+    }
+}
+
+if (isset($_GET["json"])) {
+    index($data, false);
+    echo json($data);
+} elseif (isset($_GET["ajax"])) {
+    $data["ajax"] = true;
+    echo index($data, false);
+} else {
+    index($data);
+}
